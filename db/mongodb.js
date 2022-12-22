e1de8ba6d1f27920eb14888b19c34ed18ba46fb3
Victor villarroel solis <vicoenviro@gmail.com>
2022-12-21 22:27:50 -0400

    conectar a la bd II

--- db/mongodb.js
+++ db/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const init = async () => {
   try {
-    const URL = "mongodb+srv://vicoenviro:<password>@cluster0.h9yprjj.mongodb.net/?retryWrites=true&w=majority"
+    const URL = "mongodb+srv://vicoenviro:Metallica1002@cluster0.h9yprjj.mongodb.net/ecommerce?retryWrites=true&w=majority"
     await mongoose.connect(URL)
     console.log('Database connected.')
   } catch (error) {

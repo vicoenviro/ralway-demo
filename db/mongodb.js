9395ce713b4ced3aa14a83dd6a5deb111b0682f6
Victor villarroel solis <vicoenviro@gmail.com>
2022-12-21 22:14:46 -0400

    conectar a la bd

--- db/mongodb.js
+++ db/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 export const init = async () => {
   try {
-    const URL = process.env.MONGODB_URI
+    const URL = "mongodb+srv://vicoenviro:<password>@cluster0.h9yprjj.mongodb.net/?retryWrites=true&w=majority"
     await mongoose.connect(URL)
     console.log('Database connected.')
   } catch (error) {
